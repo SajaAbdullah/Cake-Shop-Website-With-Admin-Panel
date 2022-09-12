c0d5873e5ead95acc104cf9532a792419428886a
Saja Abdullah <sajood6667@gmail.com>
2022-09-13 00:40:09 +0500

    ProjectOutput_Upload

---
+++ customer-frontend/.next/static/webpack/pages/about.afd32228946db7fc.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/about",{
+
+/***/ "./src/data/testimonial/testimonial.json":
+/*!***********************************************!*\
+  !*** ./src/data/testimonial/testimonial.json ***!
+  \***********************************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"name":"Zonaira Kamran","image":"/assets/img/comment-author2.png","id":12,"body":"Really good experience from point of ordering. Cake delivered in time and very well packaged. The cake was beautifully decorated and tasted great"},{"name":"Ayesha Zaheer","image":"/assets/img/comment-author2.png","id":15,"body":"Perfect cake! Exactly like in the picture! Everybody loved it and it was delicious. Definitely will be back next year!"}]');
+
+/***/ })
+
+});
